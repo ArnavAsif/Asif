2eecf329ba642556fa37ce75c0a9632f60d204f9
Arnav Asif <ifas092@gmail.com>
2026-08-08 20:45:43 +0600

    Project updated

---
+++ .vercel/output/functions/__server.func/_slug-C_V4u0OW.mjs
@@ -0,0 +1,16 @@
+import { t as getProjectBySlug } from "./_ssr/projects-CBNklGFW.mjs";
+import { f as lazyRouteComponent, p as createFileRoute } from "./_libs/@tanstack/react-router+[...].mjs";
+//#region node_modules/.nitro/vite/services/ssr/assets/_slug-C_V4u0OW.js
+var $$splitComponentImporter = () => import("./_slug-GuPgSwnK.mjs");
+var Route = createFileRoute("/projects/$slug")({
+	head: ({ params }) => {
+		const project = getProjectBySlug(params.slug);
+		return { meta: [{ title: project ? `${project.name} — Project Details` : "Project Not Found" }, {
+			name: "description",
+			content: project?.description ?? ""
+		}] };
+	},
+	component: lazyRouteComponent($$splitComponentImporter, "component")
+});
+//#endregion
+export { Route as t };
---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BOC9KmUT.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BOC9KmUT.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "D:/projects/Asif/src/routes/__root.tsx",
+		children: ["/", "/projects/$slug"],
+		preloads: ["/assets/index-DQcWTW6d.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DQcWTW6d.js"
+		} }]
+	},
+	"/": {
+		filePath: "D:/projects/Asif/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-FPanxJVs.js", "/assets/use-hover-scroll-DzdWezBq.js"]
+	},
+	"/projects/$slug": {
+		filePath: "D:/projects/Asif/src/routes/projects/$slug.tsx",
+		children: void 0,
+		preloads: ["/assets/_slug-C0sb9zse.js", "/assets/use-hover-scroll-DzdWezBq.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
